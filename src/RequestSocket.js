61678985004bed395259d05cc72ebb8044cc3694
Owner <minemidnightmc@gmail.com>
2019-02-10 22:13:49 -0500

    more debug :(

--- src/RequestSocket.js
+++ src/RequestSocket.js
@@ -12,6 +12,7 @@ class RPCSocket {
 	start(proto) {
 		this.proto = proto;
 
+		console.log(`tcp://discord-bucket-zmq-proxy:${process.env.DISCORD_BUCKET_ZMQ_PROXY_SERVICE_PORT_DEALER}`);
 		this.socket.connect(`tcp://discord-bucket-zmq-proxy:${process.env.DISCORD_BUCKET_ZMQ_PROXY_SERVICE_PORT_DEALER}`);
 	}
 
