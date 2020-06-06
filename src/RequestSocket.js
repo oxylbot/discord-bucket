2a789942bd55dc761a998b2015f04e049dc4568b
minemidnight <minemidnightmc@gmail.com>
2020-06-06 02:56:20 -0400

    Upgrade to zeromq v6

--- src/RequestSocket.js
+++ src/RequestSocket.js
@@ -1,10 +1,10 @@
 const handler = require("./request-handler");
-const zmq = require("zeromq");
+const { Router } = require("zeromq");
 
 class RPCSocket {
 	constructor() {
-		this.socket = zmq.socket("router");
-		this.socket.on("message", this.message.bind(this));
+		this.socket = new Router();
+		this.messageHandler();
 
 		this.proto = null;
 	}
@@ -19,24 +19,27 @@ class RPCSocket {
 		this.socket.close();
 	}
 
-	async message(proxy, client, message) {
-		const request = this.proto.rpc.lookup("Request");
-		const decoded = request.decode(message);
+	async messageHandler() {
+		while(!this.socket.closed) {
+			const [proxy, client, message] = await this.socket.receive();
+			const request = this.proto.rpc.lookup("Request");
+			const decoded = request.decode(message);
 
-		const requestType = this.proto.discord.lookup(decoded.requestType);
-		const result = await handler(decoded.name, requestType.decode(decoded.data));
+			const requestType = this.proto.discord.lookup(decoded.requestType);
+			const result = await handler(decoded.name, requestType.decode(decoded.data));
 
-		const responseType = this.proto.discord.lookup(result.responseType);
-		const verifyError = responseType.verify(result.data);
-		if(verifyError) throw new Error(verifyError);
+			const responseType = this.proto.discord.lookup(result.responseType);
+			const verifyError = responseType.verify(result.data);
+			if(verifyError) throw new Error(verifyError);
 
-		const buffer = this.proto.rpc.lookup("Response").encode({
-			id: decoded.id,
-			responseType: result.responseType,
-			data: responseType.encode(responseType.fromObject(result.data)).finish()
-		}).finish();
+			const buffer = this.proto.rpc.lookup("Response").encode({
+				id: decoded.id,
+				responseType: result.responseType,
+				data: responseType.encode(responseType.fromObject(result.data)).finish()
+			}).finish();
 
-		this.socket.send([proxy, client, buffer]);
+			await this.socket.send([proxy, client, buffer]);
+		}
 	}
 }
 
